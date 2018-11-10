97db005f6b1d6589e073b48a307db15202ae011b
essem <soomin@gmail.com>
2018-11-10 19:20:07 +0900

    Add basic routing

--- apps/kv/config/config.exs
+++ apps/kv/config/config.exs
@@ -21,6 +21,8 @@ use Mix.Config
 #     config :logger, level: :info
 #
 
+config :kv, :routing_table, [{?a..?m, :"foo@computer-name"}, {?n..?z, :"bar@computer-name"}]
+
 # It is also possible to import configuration files, relative to this
 # directory. For example, you can emulate configuration per environment
 # by uncommenting the line below and defining dev.exs, test.exs and such.
